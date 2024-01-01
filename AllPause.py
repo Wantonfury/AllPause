4b08f600b3418f854b4f47b2142150204f5d2aa6
Wantonfury <orzan.alexandru94@gmail.com>
2024-01-01 15:08:12 +0200

    Add saving and loading of settings

--- AllPause.py
+++ AllPause.py
@@ -1,21 +1,31 @@
 import psutil
 import eel
 
-# Global values (for debug atm)
-KEY_PAUSE = 13
+def saveSettings(keys):
+  with open("settings.txt", "w") as settings:
+    for key in keys:
+      settings.write(key[0] + " " + str(key[1]) + " " + str(key[2]) + " " + str(key[3]) + "\n")
 
-def loadSettings():
+def loadSettings(keys):
   with open("settings.txt", "r") as settings:
     lines = settings.readlines()
+    
+    if len(lines) <= 0:
+      keys.append(["All", 17, 16, 19])
+      return
     
     for line in lines:
       line = line.replace("\n", "")
       columns = line.split(" ")
-      print(columns)
+      keys.append([columns[0], int(columns[1]), int(columns[2]), int(columns[3])])
 
 def init():
+  keys = []
+  
   # Load saved settings
-  loadSettings()
+  loadSettings(keys)
+  print(keys)
+  saveSettings(keys)
   
   # Initialize GUI
   eel.init("web")
